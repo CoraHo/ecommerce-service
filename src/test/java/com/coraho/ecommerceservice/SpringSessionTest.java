d9577e60297ec79ad877e5cf5899740c5a9748e7
CoraHo <cora9122@gmail.com>
2026-01-23 16:21:50 -0500

    Fixed sessionResposirtoy bean not found issue

--- src/test/java/com/coraho/ecommerceservice/SpringSessionTest.java
+++ src/test/java/com/coraho/ecommerceservice/SpringSessionTest.java
@@ -5,19 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.session.FindByIndexNameSessionRepository;
 import org.springframework.session.Session;
-import org.springframework.session.SessionRepository;
 
 @SpringBootTest
 public class SpringSessionTest {
 
     @Autowired
-    private SessionRepository<Session> sessionRepository;
+    private FindByIndexNameSessionRepository<? extends Session> sessionRepository;
 
     @Test
     void testSpringSessionIsConfigured() {
         assertNotNull(sessionRepository, "Spring session is not configured");
-        System.out.println("Session Repository type: " + sessionRepository.getClass().getName());
+        System.out.println("Session Repository type: " +
+                sessionRepository.getClass().getName());
     }
 
 }
