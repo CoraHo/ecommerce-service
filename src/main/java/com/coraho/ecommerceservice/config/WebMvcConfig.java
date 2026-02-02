2a9c5a80e43c919ac504e3e74bfb1ab60661f07f
CoraHo <cora9122@gmail.com>
2026-02-02 15:57:27 -0500

    removed unused inports

--- src/main/java/com/coraho/ecommerceservice/config/WebMvcConfig.java
+++ src/main/java/com/coraho/ecommerceservice/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.coraho.ecommerceservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
