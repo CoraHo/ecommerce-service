a236dc99fbc436889e18bf9387b8c0ae5d9ed398
CoraHo <cora9122@gmail.com>
2026-02-09 17:30:09 -0500

    Started writing pure unit tests for CustomUserDetailsService

---
+++ src/test/java/com/coraho/ecommerceservice/security/CustomUserDetailsServiceTest.java
@@ -0,0 +1,11 @@
+package com.coraho.ecommerceservice.security;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("CustomUserDetailsService Tests")
+public class CustomUserDetailsServiceTest {
+
+}
